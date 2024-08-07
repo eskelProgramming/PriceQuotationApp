4cf315b055cc3802d8663042a7c9d20a1c07c805
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-08-07 12:51:27 -0800

    Changed error messages

--- PriceQuotationApp/Models/Price.cs
+++ PriceQuotationApp/Models/Price.cs
@@ -4,10 +4,10 @@ namespace PriceQuotationApp.Models
 {
     public class Price
     {
-        [Range(0, double.MaxValue)]
+        [Range(0, double.MaxValue, ErrorMessage = "The Subtotal must be a positive number")]
         public double Subtotal { get; set; }
 
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "The Discount Percent must be between 0 and 100")]
         public int DiscountPercent { get; set; }
 
         public double DiscountAmount { get; private set; } = 0.00;
