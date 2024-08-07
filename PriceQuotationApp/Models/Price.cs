f80cb4a004c1e608612cc807d89336e1bbfba54f
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-08-06 16:43:23 -0800

    Added Price Model

---
+++ PriceQuotationApp/Models/Price.cs
@@ -0,0 +1,20 @@
+﻿namespace PriceQuotationApp.Models
+{
+    public class Price
+    {
+        public double Subtotal { get; set; }
+
+        public int DiscountPercent { get; set; }
+
+        public double DiscountAmount { get; private set; }
+
+        public double Total { get; private set; }
+
+        public void Calculate()
+        {
+            DiscountAmount = Subtotal * DiscountPercent / 100;
+
+            Total = Subtotal - DiscountAmount;
+        }
+    }
+}
